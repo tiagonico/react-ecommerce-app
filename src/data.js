97faf67e121a1e2ca655f7c1d8ccd5d13946f9ca
Tiago Silva <tiagosilva@multicast.com.br>
2022-11-01 17:02:47 -0300

    Adding Slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://www.pngplay.com/wp-content/uploads/8/Women-Model-Posing-PNG-HD-Quality.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://www.pngplay.com/wp-content/uploads/8/Women-Model-Transparent-File.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://www.pngplay.com/wp-content/uploads/8/Women-Model-Posing-Transparent-Images.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
