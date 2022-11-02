6f6f815b19add965d2e70667d9ca3d266a7d242e
Tiago Silva <tiagosilva@multicast.com.br>
2022-11-02 15:23:23 -0300

    Adding Login and Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Product from "./pages/Product"
+import Login from "./pages/Login"
+//import Product from "./pages/Product"
+//import Register from "./pages/Register"
 //import Home from "./pages/Home"
 //import ProductList from "./pages/ProductList"
 
 const App = () => {
-  return <Product />
+  return <Login />
 }
 
 export default App
