4c4a0f04a1b146c0cda6197930ca5e20722f49db
Tiago Silva <tiagosilva@multicast.com.br>
2022-11-06 11:58:24 -0300

    Adding React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
-//import Cart from "./pages/Cart"
-//import Login from "./pages/Login"
-//import Product from "./pages/Product"
-//import Register from "./pages/Register"
+import Cart from "./pages/Cart"
+import Login from "./pages/Login"
+import Product from "./pages/Product"
+import Register from "./pages/Register"
 import Home from "./pages/Home"
-//import ProductList from "./pages/ProductList"
+import ProductList from "./pages/ProductList"
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 const App = () => {
-  return <Home />
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}  />
+      </Routes>
+      <Routes>
+        <Route path="/products/:category" element={<ProductList/>}  />
+      </Routes>
+      <Routes>
+        <Route path="/product/:id" element={<Product/>}  />
+      </Routes>
+      <Routes>
+        <Route path="/cart" element={<Cart/>}  />
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login/>}  />
+      </Routes>
+      <Routes>
+        <Route path="/register" element={<Register/>}  />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
